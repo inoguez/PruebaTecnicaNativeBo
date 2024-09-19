bf5aefb21bd38e14efb32ce02fc9fe9f380ae041
Ian <ian@inein.mx>
2024-09-19 17:36:54 -0600

    Added workflows to cd/ci

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
+	output: "static",
+	site: "https://prueba-tecnica-navite-bo.github.io",
+	base: "PruebaTecnicaNativeBo",
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+	],
 });
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export default function NavBar() {
-  return (
-    <nav className='flex gap-2 text-lg justify-center items-center h-full'>
-      <a href='/'>Home</a>
-      <a href='/favorites'>Favorites</a>
-    </nav>
-  );
+	return (
+		<nav className="flex gap-2 text-lg justify-center items-center h-full">
+			<a href="/PruebaTecnicaNativeBo/">Home</a>
+			<a href="/PruebaTecnicaNativeBo/favorites">Favorites</a>
+		</nav>
+	);
 }
